3a665294c60fa91ade8a204e5894284cb0739d58
Jinho Yoon <yoonjinho0318@gmail.com>
2023-11-15 22:34:20 -0500

    style: ormolu format

--- main.hs
+++ main.hs
@@ -1,8 +1,9 @@
+import Data.Function (on)
 import Data.List (intercalate, maximumBy, minimumBy)
 import Data.Maybe (isNothing)
-import Data.Function (on)
 
 data Tile = O | X deriving (Show, Eq)
+
 data Result = Win | Loss | Draw deriving (Show)
 
 type Board = [(Int, Tile)]
@@ -14,6 +15,7 @@ elems indices board = map (`lookup` board) indices
 allLines line board = map (`line` board) [0 .. 2]
 
 rows = allLines row where row n = elems [(n * 3) .. (n * 3) + 2]
+
 cols = allLines col where col n = elems [n, n + 3, n + 6]
 
 (dia1, dia2) = (elems [0, 4, 8], elems [2, 4, 6])
@@ -22,12 +24,11 @@ cols = allLines col where col n = elems [n, n + 3, n + 6]
 showBoard :: Board -> String
 showBoard = unlines . map (unwords . map showTile) . rows
   where
-    showTile Nothing  = "_"
+    showTile Nothing = "_"
     showTile (Just O) = "O"
     showTile (Just X) = "X"
 
 printBoard = putStr . showBoard
-
 
 -- Utility functions
 score :: Result -> Int
@@ -36,9 +37,9 @@ score Draw = 0
 score Loss = -1
 
 turn :: Board -> Tile
-turn board =
-  if numTiles O board == numTiles X board then O else X
-  where numTiles tile = length . filter ((== tile) . snd)
+turn board = if numTiles O board == numTiles X board then O else X
+  where
+    numTiles tile = length . filter ((== tile) . snd)
 
 next :: Tile -> Tile
 next O = X
@@ -46,23 +47,22 @@ next X = O
 
 finished :: Board -> Maybe Result
 finished board
-  | check O board     = Just Win
-  | check X board     = Just Loss
+  | check O board = Just Win
+  | check X board = Just Loss
   | length board == 9 = Just Draw
-  | otherwise         = Nothing
+  | otherwise = Nothing
   where
     check tile board =
-      let allTile = all (== Just tile) in
-      any allTile (rows board ++ cols board ++ [dia1 board, dia2 board])
-
+      let allTile = all (== Just tile)
+       in any allTile (rows board ++ cols board ++ [dia1 board, dia2 board])
 
 -- Minimax Algorithm:
 -- Helper function: Possible moves
 possibleMoves :: Tile -> Board -> [Board]
 possibleMoves turn board =
-  map ((: board) . (, turn)) . filter (isNothing . (`lookup` board)) $ [0 .. 8]
+  map ((: board) . (,turn)) . filter (isNothing . (`lookup` board)) $ [0 .. 8]
 
-minimax :: Tile -> Board -> Int 
+minimax :: Tile -> Board -> Int
 minimax turn board =
   case finished board of
     Just result -> score result
@@ -71,7 +71,6 @@ minimax turn board =
         O -> maximum . map (minimax X) $ possibleMoves O board
         X -> minimum . map (minimax O) $ possibleMoves X board
 
-
 -- Play the game
 play :: Tile -> Board -> IO Board
 play turn board = do
@@ -79,10 +78,11 @@ play turn board = do
   return ((read coord :: Int, turn) : board)
 
 playAI :: Tile -> Board -> IO Board
-playAI X = do
-  return
-  . minimumBy (compare `on` minimax O)
-  . possibleMoves X
+playAI X =
+  do
+    return
+    . minimumBy (compare `on` minimax O)
+    . possibleMoves X
 
 game :: Tile -> Board -> IO ()
 game turn board =
@@ -90,9 +90,10 @@ game turn board =
     Just result -> putStrLn (show result ++ "!")
     Nothing -> do
       putStrLn (show turn ++ "'s turn:")
-      board <- do 
-        if turn == O then play turn board
-        else playAI turn board
+      board <- do
+        if turn == O
+          then play turn board
+          else playAI turn board
       printBoard board
       game (next turn) board
 
