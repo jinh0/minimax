0fb85dae74536f3fe1c2ee394b323de748ce9d23
Jinho Yoon <yoonjinho0318@gmail.com>
2023-11-23 15:46:03 -0500

    refactor: rename Tile to Player

--- main.hs
+++ main.hs
@@ -2,14 +2,14 @@ import Data.Function (on)
 import Data.List (intercalate, maximumBy, minimumBy)
 import Data.Maybe (isNothing)
 
-data Tile = O | X deriving (Show, Eq)
+data Player = O | X deriving (Show, Eq)
 
 data Result = Win | Loss | Draw deriving (Show)
 
-type Board = [(Int, Tile)]
+type Board = [(Int, Player)]
 
 -- Access elements
-elems :: [Int] -> Board -> [Maybe Tile]
+elems :: [Int] -> Board -> [Maybe Player]
 elems indices board = map (`lookup` board) indices
 
 allLines line board = map (`line` board) [0 .. 2]
@@ -36,12 +36,15 @@ score Win = 1
 score Draw = 0
 score Loss = -1
 
-turn :: Board -> Tile
-turn board = if numTiles O board == numTiles X board then O else X
+turn :: Board -> Player
+turn board =
+  if numTiles O board == numTiles X board
+    then O
+    else X
   where
     numTiles tile = length . filter ((== tile) . snd)
 
-next :: Tile -> Tile
+next :: Player -> Player
 next O = X
 next X = O
 
@@ -54,37 +57,37 @@ finished board
   where
     check tile board =
       let allTile = all (== Just tile)
-       in any allTile (rows board ++ cols board ++ [dia1 board, dia2 board])
+      in any allTile (rows board ++ cols board ++ [dia1 board, dia2 board])
 
 -- Minimax Algorithm:
 -- Helper function: Possible moves
-possibleMoves :: Tile -> Board -> [Board]
+possibleMoves :: Player -> Board -> [Board]
 possibleMoves turn board =
   map ((: board) . (,turn)) . filter (isNothing . (`lookup` board)) $ [0 .. 8]
 
-minimax :: Tile -> Board -> Int
+minimax :: Player -> Board -> Int
 minimax turn board =
   case finished board of
     Just result -> score result
     Nothing ->
       case turn of
-        O -> maximum . map (minimax X) $ possibleMoves O board
-        X -> minimum . map (minimax O) $ possibleMoves X board
+        O -> maximum . map (minimax X) . possibleMoves O $ board
+        X -> minimum . map (minimax O) . possibleMoves X $ board
 
 -- Play the game
-play :: Tile -> Board -> IO Board
+play :: Player -> Board -> IO Board
 play turn board = do
   coord <- getLine
   return ((read coord :: Int, turn) : board)
 
-playAI :: Tile -> Board -> IO Board
+playAI :: Player -> Board -> IO Board
 playAI X =
   do
     return
     . minimumBy (compare `on` minimax O)
     . possibleMoves X
 
-game :: Tile -> Board -> IO ()
+game :: Player -> Board -> IO ()
 game turn board =
   case finished board of
     Just result -> putStrLn (show result ++ "!")
