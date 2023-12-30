d96c619fb5025fbea2ec2b49df5d57ea25b2df0f
Jinho Yoon <yoonjinho0318@gmail.com>
2023-12-30 12:25:17 -0500

    feat: add alpha-beta pruning

--- minimax.hs
+++ minimax.hs
@@ -25,9 +25,10 @@ class Minimax b where
   tree turn board =
     case finished board of
       Just result -> Node{val = score result, children = [], board = board}
-      Nothing -> Node{val = maximum (map val children), children = children, board = board}
+      Nothing -> Node{val = optimize (map val children), children = children, board = board}
         where
           children = map (tree (next turn)) (possibleMoves turn board)
+          optimize = if turn == O then maximum else minimum
 
   leaves :: MTree b -> Int
   leaves tree = (if null (children tree) then 1 else 0) + sum (map leaves (children tree))
@@ -36,8 +37,36 @@ class Minimax b where
   playAI turn =
     do
       return
-      . optimizeBy turn (compare `on` minimax (next turn))
+      . optimizeBy turn (compare `on` minimax'' (next turn))
       . possibleMoves turn
     where
       optimizeBy O = maximumBy
       optimizeBy X = minimumBy
+
+  minimax'' :: Player -> b -> Int
+  minimax'' = minimax' (-3) 3
+
+  minimax' :: Int -> Int -> Player -> b -> Int
+  minimax' alpha beta turn board =
+    case finished board of
+      Just result -> score result
+      Nothing ->
+        case turn of
+          O -> maxval alpha beta board
+          X -> minval alpha beta board
+
+-- maxval :: Minimax b => Int -> Int -> b -> Int
+maxval alpha beta =
+  maximum
+    . takeWhileIncl (< beta)
+    . scanl (\a board -> max a (minimax' a beta X board)) alpha
+    . possibleMoves O
+
+minval alpha beta =
+  minimum
+    . takeWhileIncl (> alpha)
+    . scanl (\b board -> min b (minimax' alpha b O board)) beta
+    . possibleMoves X
+
+takeWhileIncl _ [] = []
+takeWhileIncl p (x : xs) = x : if p x then takeWhileIncl p xs else []
