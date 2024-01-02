d943634ec1c34f5ede82f3421ec928b18f107eeb
Jinho Yoon <yoonjinho0318@gmail.com>
2024-01-02 18:18:34 -0500

    test: add tester

--- src/tester.hs
+++ src/tester.hs
@@ -1,10 +1,18 @@
 module Tester where
 
--- import Common
--- import Control.Monad.Writer
--- import Data.Functor.Sum
--- import Minimax
--- import Tictactoe.Game
---
--- logMinimax :: Minimax b => Player -> b -> Writer (Sum Int) Int
--- logMinimax turn board = tell (Sum 1) >> return (minimax turn board)
+import Common
+import Control.Monad.Writer
+import Minimax
+
+minimax'' :: Minimax b => Player -> b -> (Int, Int)
+minimax'' turn board =
+  case finished board of
+    Just result -> (score result, 1)
+    Nothing ->
+      case turn of
+        O ->
+          let children = map (minimax'' X) . possibleMoves O $ board
+           in (maximum (map fst children), 1 + sum (map snd children))
+        X ->
+          let children = map (minimax'' O) . possibleMoves X $ board
+           in (minimum (map fst children), 1 + sum (map snd children))
